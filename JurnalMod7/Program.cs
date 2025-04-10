9bce59d8a7202b77d7dd487e5019cf0382ad2d48
aprilHardinata <aprilhardihardi439@gmail.com>
2025-04-10 10:52:49 +0700

    menambahkan class GlossaryItem103022300027

--- JurnalMod7/Program.cs
+++ JurnalMod7/Program.cs
@@ -1,7 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 DataMahasiswa103022300027 dataHandler = new DataMahasiswa103022300027();
 dataHandler.ReadJSON();
-
-TeamMembers103022300027 kuliahHandler = new TeamMembers103022300027();
-kuliahHandler.ReadJSON();
+Console.WriteLine("");
+TeamMembers103022300027 teamsHandler = new TeamMembers103022300027();
+teamsHandler.ReadJSON();
+Console.WriteLine("");
+GlossaryItem103022300027 glossaryHandler = new GlossaryItem103022300027();
+glossaryHandler.ReadJSON();
+Console.WriteLine("");
 Console.WriteLine("\nTekan Enter untuk keluar...");
