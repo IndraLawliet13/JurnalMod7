7ffe7b7729933c7aea318d66965d83f21023f278
Indra Yuda <indralawliet13@gmail.com>
2025-04-10 10:19:26 +0700

    menambahkan class TeamMembers10302230017

--- JurnalMod7/Program.cs
+++ JurnalMod7/Program.cs
@@ -4,5 +4,7 @@
     {
         DataMahasiswa103022300017 dataMahasiswa = new DataMahasiswa103022300017();
         dataMahasiswa.ReadJSON();
+        TeamMembers10302230017 teamMembers = new TeamMembers10302230017();
+        teamMembers.ReadJSON();
     }
 }
