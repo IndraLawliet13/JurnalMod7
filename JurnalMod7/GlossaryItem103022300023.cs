8a4330eccdf7325a7a3b71a3b1d2f7290f7011fb
Muhammad Azwa Radya Razadisya <137063182+piqxxu@users.noreply.github.com>
2025-04-10 11:18:09 +0700

    Menambahkan Class GlossaryItem103022300023 (Belum selesai)

---
+++ JurnalMod7/GlossaryItem103022300023.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace JurnalMod7
+{
+
+    public class GlossDef10302230023
+    {
+        public string para { get; set; }
+        public List<string> GlossSeeAlso { get; set; }
+    }
+
+    public class GlossEntry103022300023
+    {
+        public string ID { get; set; }
+        public string SortAs { get; set; }
+        public string GlossTerm { get; set; }
+        public string Acronym { get; set; }
+        public string Abbrev { get; set; }
+        public GlossDef10302230023 GlossDef { get; set; }
+
+    }
+
+    public class GlossList103022300023
+    {
+        public GlossEntry103022300023 glossEntry { get; set; }
+    }
+
+    public class GlossDiv103022300023
+    {
+        public string title { get; set; }
+        public GlossList103022300023 glossList { get; set; }
+    }
+
+    public class glossaryItem103022300023
+    {
+        public string title { get; set; }
+        public GlossDiv103022300023 GlossDiv { get; set; }
+    }
+
+    public class glossary103022300023
+    {
+        public glossary103022300023 glossary2 { get; set; }
+    }
+    public class GlossaryItem103022300023
+    {
+        public void ReadJSON()
+        {
+            string jsonName = "jurnal7_3_103022300023.json";
+            try
+            {
+                string jsonString = File.ReadAllText(jsonName);
+                glossary103022300023 glossary = JsonSerializer.Deserialize<glossary103022300023>(jsonString);
+            }
+        }
+    }
+}
--- JurnalMod7/Program.cs
+++ JurnalMod7/Program.cs
@@ -5,3 +5,6 @@ data1.ReadJSON();
 Console.WriteLine("\n===================================\n");
 TeamMembers103022300023 team = new TeamMembers103022300023();
 team.ReadJSON();
+Console.WriteLine("\n===================================\n");
+GlossaryItem103022300023 gloss = new GlossaryItem103022300023();
+GlossaryItem103022300023.ReadJSON();
