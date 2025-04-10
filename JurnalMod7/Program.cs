39c31741b86b0acac90e81f6812fc26f86661b2e
aprilHardinata <aprilhardihardi439@gmail.com>
2025-04-10 10:20:18 +0700

    menambahkan class teamsMember103022300027

--- JurnalMod7/Program.cs
+++ JurnalMod7/Program.cs
@@ -1,3 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 DataMahasiswa103022300027 dataHandler = new DataMahasiswa103022300027();
 dataHandler.ReadJSON();
+
+TeamMembers103022300027 kuliahHandler = new TeamMembers103022300027();
+kuliahHandler.ReadJSON();
+Console.WriteLine("\nTekan Enter untuk keluar...");
