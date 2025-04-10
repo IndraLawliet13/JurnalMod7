09304034a44a0a2e1a01e3c2418b6ddcad88c86c
maxswin99 <andreaschristianfirga06@gmail.com>
2025-04-10 11:09:32 +0700

    menambahkan class GlossaryItem103022300002_AndreasChristianFIrga

--- JurnalMod7/GlossaryItem103022300002_AndreasChristianFIrga.cs
+++ JurnalMod7/GlossaryItem103022300002_AndreasChristianFIrga.cs
@@ -3,43 +3,43 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text.Json;
 
-public class GlossDef103022300017
+public class GlossDef103022300002
 {
     public string para { get; set; }
     public List<string> GlossSeeAlso { get; set; }
 }
 
-public class GlossEntry103022300017
+public class GlossEntry103022300002
 {
     public string ID { get; set; }
     public string SortAs { get; set; }
     public string GlossTerm { get; set; }
     public string Acronym { get; set; }
     public string Abbrev { get; set; }
-    public GlossDef103022300017 GlossDef { get; set; }
+    public GlossDef103022300002 GlossDef { get; set; }
     public string GlossSee { get; set; }
 }
 
-public class GlossList103022300017
+public class GlossList103022300002
 {
-    public GlossEntry103022300017 GlossEntry { get; set; }
+    public GlossEntry103022300002 GlossEntry { get; set; }
 }
 
-public class GlossDiv103022300017
+public class GlossDiv103022300002
 {
     public string title { get; set; }
-    public GlossList103022300017 GlossList { get; set; }
+    public GlossList103022300002 GlossList { get; set; }
 }
 
-public class glosarryItem103022300017
+public class GlossaryItemData103022300002
 {
     public string title { get; set; }
-    public GlossDiv103022300017 GlossDiv { get; set; }
+    public GlossDiv103022300002 GlossDiv { get; set; }
 }
 
-public class glosarry103022300017
+public class GlossaryRoot103022300002
 {
-    public glosarryItem103022300017 glossary { get; set; }
+    public GlossaryItemData103022300002 glossary { get; set; }
 }
 
 public class GlossaryItem103022300002
@@ -50,7 +50,8 @@ public class GlossaryItem103022300002
         try
         {
             string jsonString = File.ReadAllText(jsonName);
-            glosarry103022300017 glossary = JsonSerializer.Deserialize<glosarry103022300017>(jsonString);
+            GlossaryRoot103022300002 glossary = JsonSerializer.Deserialize<GlossaryRoot103022300002>(jsonString);
+
             Console.WriteLine("Glossary title: " + glossary.glossary.title);
             Console.WriteLine("Glossary ID: " + glossary.glossary.GlossDiv.GlossList.GlossEntry.ID);
             Console.WriteLine("Glossary SortAs: " + glossary.glossary.GlossDiv.GlossList.GlossEntry.SortAs);
@@ -58,13 +59,12 @@ public class GlossaryItem103022300002
             Console.WriteLine("Glossary Acronym: " + glossary.glossary.GlossDiv.GlossList.GlossEntry.Acronym);
             Console.WriteLine("Glossary Abbrev: " + glossary.glossary.GlossDiv.GlossList.GlossEntry.Abbrev);
             Console.WriteLine("Glossary GlossDef para: " + glossary.glossary.GlossDiv.GlossList.GlossEntry.GlossDef.para);
-            Console.WriteLine("Glossary GlossDef GlossSeeAlso: ");
-            for (int i = 0; i < glossary.glossary.GlossDiv.GlossList.GlossEntry.GlossDef.GlossSeeAlso.Count; i++)
+            Console.WriteLine("Glossary GlossDef GlossSeeAlso:");
+            foreach (var item in glossary.glossary.GlossDiv.GlossList.GlossEntry.GlossDef.GlossSeeAlso)
             {
-                Console.WriteLine(glossary.glossary.GlossDiv.GlossList.GlossEntry.GlossDef.GlossSeeAlso[i]);
+                Console.WriteLine(" - " + item);
             }
             Console.WriteLine("Glossary GlossSee: " + glossary.glossary.GlossDiv.GlossList.GlossEntry.GlossSee);
-
         }
         catch (Exception ex)
         {
